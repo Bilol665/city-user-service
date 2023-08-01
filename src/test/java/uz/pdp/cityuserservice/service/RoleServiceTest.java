d600e6c52314d2d9192ecdf14b61252c4c54948b
Fazliddin <dilshodovfazliddin@gmail.com>
2023-08-02 00:08:26 +0500

    I wrote a junit test for the role Creat methods and everything is working 👌

---
+++ src/test/java/uz/pdp/cityuserservice/service/RoleServiceTest.java
@@ -0,0 +1,60 @@
+package uz.pdp.cityuserservice.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
+import uz.pdp.cityuserservice.domain.dto.RoleDto;
+import uz.pdp.cityuserservice.domain.entity.user.PermissionEntity;
+import uz.pdp.cityuserservice.domain.entity.user.RoleEntity;
+import uz.pdp.cityuserservice.repository.user.PermissionRepository;
+import uz.pdp.cityuserservice.repository.user.RoleRepository;
+import uz.pdp.cityuserservice.service.role.RoleService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+
+class RoleServiceTest {
+    private final String name = "Tester";
+    private RoleDto roleDto;
+    private RoleEntity roleEntity;
+    @Mock
+    private PermissionRepository permissionRepository;
+    @Mock
+    private ModelMapper modelMapper;
+    @Mock
+    private RoleRepository roleRepository;
+    @InjectMocks
+    private RoleService roleService;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+        PermissionEntity tester = permissionRepository.save(PermissionEntity.builder().permission("TESTER").build());
+        roleDto = new RoleDto(name, List.of("TESTER"));
+        roleEntity = RoleEntity.builder()
+                .role(name)
+                .permissions(List.of(tester))
+                .build();
+    }
+
+
+    @Test
+    void saveRoleTest(){
+        when(modelMapper.map(roleDto,RoleEntity.class)).thenReturn(roleEntity);
+        when(roleRepository.save(roleEntity)).thenReturn(roleEntity);
+
+        RoleEntity role = roleService.save(roleDto);
+
+
+        assertEquals(name,role.getRole());
+
+    }
+
+}
