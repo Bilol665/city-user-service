b202ad6a979f84876767a34484fe764a58b0f68f
Azamat <hasanjonovazamat@gmail.com>
2023-08-04 10:32:09 +0500

    Minor improvements

--- src/main/java/uz/pdp/cityuserservice/components/DataLoader.java
+++ src/main/java/uz/pdp/cityuserservice/components/DataLoader.java
@@ -5,12 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import uz.pdp.cityuserservice.domain.entity.user.PermissionEntity;
 import uz.pdp.cityuserservice.domain.entity.user.RoleEntity;
 import uz.pdp.cityuserservice.domain.entity.user.UserEntity;
 import uz.pdp.cityuserservice.domain.entity.user.UserState;
 import uz.pdp.cityuserservice.repository.user.PermissionRepository;
-import uz.pdp.cityuserservice.repository.user.RoleRepository;
 import uz.pdp.cityuserservice.repository.user.UserRepository;
 
 
@@ -37,7 +35,8 @@ public class DataLoader implements CommandLineRunner {
                         passwordEncoder.encode("admin"),
                         List.of(new RoleEntity("ROLE_SUPER_ADMIN", permissionRepository.findAll())),
                         permissionRepository.findAll(),
-                        UserState.ACTIVE
+                        UserState.ACTIVE,
+                        0
                 ));
             }
         }
